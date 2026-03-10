98fa65efc902476f1d4f150b44b6af5f28e450e3
Holger Steinhaus <holger.steinhaus@emobotics.dev>
2026-03-10 08:59:11 +0100

    upgrade LVGL to v9.5.0, remove obsolete color-getter shims

--- shims/lvgl_sys.h
+++ shims/lvgl_sys.h
@@ -9,10 +9,6 @@ extern "C"
 #include "../lvgl/lvgl.h"
 
     lv_color_t _LV_COLOR_MAKE(uint8_t r, uint8_t g, uint8_t b);
-    uint16_t _LV_COLOR_GET_R(lv_color_t color);
-    uint16_t _LV_COLOR_GET_G(lv_color_t color);
-    uint16_t _LV_COLOR_GET_B(lv_color_t color);
-    uint16_t _LV_COLOR_GET_A(lv_color_t color);
 
 #ifdef __cplusplus
 } /* extern "C" */
